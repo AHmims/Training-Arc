88406f20a50a4bb2e67448ed8559f56398798c93
AHmims <ali.hmims99@gmail.com>
2020-09-22 19:00:59 +0100

    mikroOrm link to db

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("testing");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "training_arc",
+    user: "root",
+    password: "123456789",
+    debug: !__prod,
+    type: "mysql",
+  });
+};
+
+main();
